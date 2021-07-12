4392fbd4236a3592fca23f7cd987e19af22e3a6a
ThaiLe189 <58130773+ThaiLe189@users.noreply.github.com>
2021-07-12 15:19:14 +0700

    Update Sort_Rectangle_LeftRight_TopBottom.cpp

--- Sort_Rectangle_LeftRight_TopBottom.cpp
+++ Sort_Rectangle_LeftRight_TopBottom.cpp
@@ -3,6 +3,8 @@ bool compare(const cv::Rect& a, const cv::Rect& b)
     float cellSize = 20.0f;
     float lCell = floorf(a.y / cellSize);
     float rCell = floorf(b.y / cellSize);
+	
+    // config 10 with your purpose
     if (abs(lCell - rCell) < 10) return a.x < b.x;
     return (lCell < rCell);
 };
